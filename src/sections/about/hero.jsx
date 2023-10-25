06fd9dfa4559119d8cafb9bac85a4170a6bb23be
winifredogbeiwi <imadeogbeiwi@gmail.com>
2023-10-25 18:22:03 +0100

    responsive ABOUT - hero section

--- src/sections/about/hero.jsx
+++ src/sections/about/hero.jsx
@@ -4,10 +4,10 @@ import assets from '../../constants/assets'
 export const Hero = () => {
     return (
         <main className=" py-20 md:py-40 flex flex-col-reverse md:flex-row md:justify-between items-center gap-16">
-            <div className="w-1/2 ">
-                <p className=' text-xl mb-2 font-medium'>About us</p>
+            <div className="md:w-1/2 ">
+                <p className=' text-xl mb-2 font-medium text-center md:text-start'>About us</p>
                 <Heading1 text="Our designs solve problems" color="text-main" />
-                <p className='mt-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                <p className='mt-8 text-center md:text-start'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
             </div>
 
             <div>
