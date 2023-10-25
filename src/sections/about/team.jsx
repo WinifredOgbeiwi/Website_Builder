2901212e80ec1dc2d099ba95705d127e1d7fa680
winifredogbeiwi <imadeogbeiwi@gmail.com>
2023-10-25 17:06:45 +0100

    ABOUT - team section

--- src/sections/about/team.jsx
+++ src/sections/about/team.jsx
@@ -7,8 +7,8 @@ export const Team = () => {
     <section className=" py-[128px] bg-[#F4F6FC] text-center">
       <Heading2 text="Meet Our Team" />
       <div className="flex flex-wrap justify-center items-center gap-10 mt-12">
-        {data.Team_data.map(({ img, position, name }) => (
-          <div className=" shadow-lg bg-white text-center w-fit px-11 py-9">
+        {data.Team_data.map(({id, img, position, name }) => (
+          <div key={id} className=" shadow-lg bg-white text-center w-fit px-11 py-9">
             <div className="w-40 h-40 rounded-full overflow-hidden">
               <img src={img} alt="" className=" object-fill" />
             </div>
