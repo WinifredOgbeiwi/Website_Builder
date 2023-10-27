01d70a6afd9886983283c63d2565a1f0fc4c0a25
winifredogbeiwi <imadeogbeiwi@gmail.com>
2023-10-27 17:28:27 +0100

    restrutured HOME- text centered in inquiry section

--- src/sections/home/inquiry.jsx
+++ src/sections/home/inquiry.jsx
@@ -16,13 +16,13 @@ const Inquiry = () => {
                     backgroundColor: 'rgba(28, 30, 83, 0.50)',
                 }}>
                     <Heading1 text="Building stellar websites for early startups" />
-                    <p className='mt-6 text-xs sm:text-base max-[350px]:text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim.</p>
+                    <p className='mt-6 text-xs sm:text-base text-center md:text-start'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim.</p>
                 </div>
             </div>
 
             <div className='bg-[#1C1E53] px-6  sm:px-12 lg:px-24 pt-16 sm:pt-24 pb-[48px] text-white md:w-1/2'>
-                <h3 className='text-3xl font-medium max-[350px]:text-center'>Send inquiry</h3>
-                <p className='text-xs sm:text-base font-medium mt-4 mb-10 max-[350px]:text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                <h3 className='text-3xl font-medium text-center md:text-start'>Send inquiry</h3>
+                <p className='text-xs sm:text-base font-medium mt-4 mb-10 text-center md:text-start'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
                 <form action="">
                     <input type="text" placeholder='Name' required />
                     <input type="email" placeholder='Email' required />
